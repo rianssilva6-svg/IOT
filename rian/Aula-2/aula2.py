357c09dac6128ab9a9e02c5c86de223bbd821369
Rian01048 <riansouzasilva247@gmail.com>
2026-08-18 16:30:07 -0300

    PYTHON

---
+++ rian/Aula-2/aula2.py
@@ -0,0 +1,11 @@
+import os
+os.system('cls')
+
+numeros = [33,11,22,54,51,52,17]
+nomes = ['Julia','Maria','Luiza','Enzo']
+
+numeros.append(67)
+numeros.insert(3,50)
+numeros.pop(2)
+
+print(numeros)
