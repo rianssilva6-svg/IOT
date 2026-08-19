c08e260b18fd68a6745f7352ed7bab360365a3fb
euZuk <notzuk679@gmail.com>
2026-08-19 16:31:36 -0300

    PYTHON

--- rian/Aula-3/while.py
+++ rian/Aula-3/while.py
@@ -1,2 +1,11 @@
+carrinho = []
+
 while True:
-    print("Eu")
+    produto = float(input("Digite o valor do produto: "))
+    if produto == 0:
+        break
+    else:
+        carrinho.append(produto)
+
+total = sum(carrinho)
+print(f"O total da compra é de R${total:.2f}")
